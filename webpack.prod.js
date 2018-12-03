ec95068c29665ebfce996ff2cd4e2f2ef3b337fb
Breno Calazans <brenocalazans@gmail.com>
2018-12-03 14:32:54 -0200

    Fix node build
    - Change webpack build to `umd` to work with an app using Webpack
    - Use babel to transpile ES6 syntax to ES5 (eg: `class`)

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,21 @@ module.exports = {
   output: {
     filename: 'boleto.min.js',
     path: path.resolve(__dirname, 'lib'),
-    library: 'Boleto'
+    library: 'Boleto',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
