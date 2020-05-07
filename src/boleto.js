81b158669e3c53790447083eb66d7c9ab7a5930e
Guilherme Araújo <guilhermeama@gmail.com>
2020-05-07 17:13:10 -0300

    Fix expiration date When converting the date to text, depending on the timezone, it could display the wrong day. This forces the reference date to be at noon BRT, which should prevent this from happening.

--- src/boleto.js
+++ src/boleto.js
@@ -151,7 +151,7 @@ class Boleto {
    * @return {Date} The expiration date of the bank slip
    */
   expirationDate() {
-    const refDate = new Date('1997-10-07');
+    const refDate = new Date('1997-10-07 12:00:00 GMT-0300');
     const days = this.barcode().substr(5, 4);
 
     return new Date(refDate.getTime() + (days * 86400000));
